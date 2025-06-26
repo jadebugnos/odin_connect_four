51acc659149306de1372aeb5c1deb11f2e8d18a8
Jade Bugnos <173978908+jadebugnos@users.noreply.github.com>
2025-06-26 08:43:26 +0800

    test: add specs for Game#play_game

---
+++ spec/game_spec.rb
@@ -0,0 +1,33 @@
+require_relative '../lib/game'
+
+RSpec.describe Game do
+  describe '#play_game' do
+    let(:game_board) { double('game_board') }
+    let(:player) { double('player') }
+    subject(:game_start) { described_class.new(player, game_board) }
+
+    before do
+      allow(game_board).to receive(:display_board)
+    end
+
+    context 'when run' do
+      it 'it sends message to display game intro' do
+        expect(game_start).to receive(:game_intro)
+        game_start.play_game
+      end
+
+      it 'displays the game_board' do
+        expect(game_board).to receive(:display_board)
+        game_start.play_game
+      end
+
+      it 'runs the game' do
+        expect(game_start).to receive(:run_game)
+        game_start.play_game
+      end
+    end
+  end
+
+  describe '#run_game' do
+  end
+end
