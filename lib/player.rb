ad94ffb64caac790d2b2b64400fc190a3a4d7c4b
Jade Bugnos <173978908+jadebugnos@users.noreply.github.com>
2025-07-01 21:10:58 +0800

    feat: add Player#player_move method
    - allows two players to take turns
    - change disc according to the current player

--- lib/player.rb
+++ lib/player.rb
@@ -1,4 +1,9 @@
-class Player
+class Player # rubocop:disable Style/Documentation
+  def initialize
+    @disc = ["|\u2648|", "|\u264C|"]
+    @last_move = []
+  end
+
   def validate_player_input
     loop do
       puts 'Select a column by typing 1-7: '
@@ -9,6 +14,16 @@ class Player
       puts 'input out of range'
     rescue ArgumentError
       puts "That's not a number. Try again."
+    end
+  end
+
+  def player_move
+    if @last_move[-1] == @disc[1] || @last_move.empty?
+      @last_move << @disc[0]
+      @disc[0]
+    else
+      @last_move << @disc[1]
+      @disc[1]
     end
   end
 end
